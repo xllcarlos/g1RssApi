4695d8c69287e2a1e6ef13838b0cba76909389db
Carlos Santana <contato.albertosantana@gmail.com>
2024-08-04 19:05:40 -0300

    feat: adicionado metodo para buscar todas noticias de uma categoria: findAllByCategory

--- src/main/java/com/g1RssApi/repositories/NoticeRepository.java
+++ src/main/java/com/g1RssApi/repositories/NoticeRepository.java
@@ -1,10 +1,12 @@
 package com.g1RssApi.repositories;
 
+import com.g1RssApi.models.CategoryModel;
 import com.g1RssApi.models.NoticeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +14,6 @@ public interface NoticeRepository extends JpaRepository<NoticeModel, Long> {
 
     Optional<NoticeModel> findById(Long id);
     Optional<NoticeModel> findByTitleAndPubDate(String name, Date pubDate);
+    List<NoticeModel> findAllByCategory(CategoryModel category);
 
 }
