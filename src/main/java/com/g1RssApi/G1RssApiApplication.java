6f5233641a04a5e7899b399a289625b4b43541e8
Carlos Santana <contato.albertosantana@gmail.com>
2024-08-04 18:51:54 -0300

    docs: adicionado comentarios

--- src/main/java/com/g1RssApi/G1RssApiApplication.java
+++ src/main/java/com/g1RssApi/G1RssApiApplication.java
@@ -3,11 +3,19 @@ package com.g1RssApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * G1 RSS API
+ * Api do portal de noticias g1 na forma de RSS.
+ *
+ * @author Jhonatan Isaias
+ * @author Carlos Santos
+ * @author Gilson Teixeira
+ */
 @SpringBootApplication
 public class G1RssApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(G1RssApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(G1RssApiApplication.class, args);
+    }
 
 }
